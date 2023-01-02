3d3d08cccda221c81b2552ad1bb8c455fffc28f0
darko905 <59891655+darko905@users.noreply.github.com>
2023-01-02 23:18:45 +0100

    Add projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -6,6 +6,24 @@ const Projects = () => {
         <section className='section projecets'>
             <h2 className='section__title'>Projekti</h2>
             <span className='section__subtitle'>Pogledajte neke od naših prjekata</span>
+
+            <div className='work__container container grid'>
+                <div className='work__card'>
+                    <img className='work__img'/>
+                    <h3 className='work__title'>Projekat br 1</h3>
+                    <p className='work__description'>
+                        “Lorem ipsum” dummy text is used by many web-developers to test how their HTML 
+                        templates will look with real data. Often, developers use third-party services 
+                        to generate “Lorem ipsum” text, but now you can do that right in your editor. 
+                        Just expand lorem or lipsum abbreviations to get the following snippet:
+                    </p>
+                    <div className='work__btn'>
+                        <a href=''>
+                            <button className='button button-flex'>View</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
